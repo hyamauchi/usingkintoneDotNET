fb20be8761af87a67899409225c737efab351ac6
Hayato Yamauchi <h.yamauchi@outlook.com>
2017-02-12 19:08:15 +0900

    プロジェクト ファイルを追加します。

---
+++ usingkintoneDotNET/BookModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using kintoneDotNET.API;
+using kintoneDotNET.API.Types;
+
+namespace usingkintoneDotNET
+{
+    public class BookModel : AbskintoneModel
+    {
+        public override string app
+        {
+            get
+            {
+                return "104";
+            }
+        }
+
+        public string age { get; set; }
+        public string job { get; set; }
+        public string contact { get; set; }
+        public string duration { get; set; }
+        public string euribor3m { get; set; }
+        public string nr_employed { get; set; }
+
+    }
+}
---
+++ usingkintoneDotNET/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace usingkintoneDotNET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<BookModel> books = BookModel.FindAll<BookModel>();
+
+            foreach (var item in books)
+            {
+                System.Diagnostics.Debug.WriteLine(item.age);
+            }
+
+        }
+    }
+}
